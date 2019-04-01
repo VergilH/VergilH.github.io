bdceca8fa378eeaeed9ca83def7b8251809ecb2e
Vergil <jjl55661122@sina.com>
2019-04-01 17:42:23 +0800

    14.6
    14.6

--- js/aside.js
+++
@@ -1,9 +0,0 @@
-var box = document.getElementById("box")
-var btn = document.getElementById("btn")
-    btn.onclick = function() {
-        if (box.offsetLeft == 0) {
-            box.style['margin-left'] = -100 + "px"
-        } else {
-            box.style['margin-left'] = 0 + "px"
-        }
-    }
